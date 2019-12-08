3e64a128bffa09a0606d4ee2372d80878df8ce72
Galus <mgalus@gmail.com>
2019-12-08 12:58:44 -0600

    Answered chal 2 part 1

--- chal02/src/main.rs
+++ chal02/src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
 use std::fs;
 
-fn is_opcode(index: usize) -> bool {
-    (index+1) & 4 == 0
+fn is_opcode_index(index: usize) -> bool {
+    return index % 4 == 0;
 }
 
 fn str_to_i32(s: &str) -> i32 {
@@ -20,38 +20,53 @@ fn main() {
     let mut program: Vec<i32> = contents.split(',')
         .map(str_to_i32)
         .collect();
-    println!("{}", format!("{:?}", program));
+    //println!("{}", format!("{:?}", program));
 
     let mut index : usize = 0;
     loop {
-        if index + 1 > program.len() { break }
-        if is_opcode(index) {
+        if 99 == program[index] {
+            println!("Finished program.");
+            let ret = format!("{:?}", program);
+            println!("Program\n{}",ret);
+            break;
+        }
+        if index >= program.len() { break }
+        if is_opcode_index(index) {
+            let result_pos : usize = program[index+3] as usize;
+            let op1 : usize = program[index+1] as usize;
+            let op2 : usize = program[index+2] as usize;
+            //println!("{} is an opcode", index);
             if 1 == program[index] {
-                program[index+3] = program[index+1] + program[index+2];
+                println!("{}:Op p[{}]+p[{}] -> {}+{} replacing p[{}]-> {} with {}",
+                    index,
+                    op1,
+                    op2,
+                    program[op1],
+                    program[op2],
+                    result_pos,
+                    program[result_pos],
+                    program[op1]+program[op2]);
+                println!("OLD: {}", format!("{:?}", program));
+                program[result_pos] = program[op1] + program[op2];
+                println!("NEW: {}", format!("{:?}", program));
             }
             if 2 == program[index] {
-                program[index+3] = program[index+1] * program[index+2]
-            }
-            if 99 == program[index] {
-                let ret = format!("{:?}", program);
-                println!("Program\n{}",ret);
-            }
-        }
-        index += 1;
-    }
-    
-    for (index, opcode) in program.iter_mut().enumerate() {
-        if is_opcode(index) {
-            if 1 == *opcode {
-                program[index+3] = program[index+1] + program[index+2]
-            }
-            if *opcode == 2 {
-                // program[index+3] = program[index+1] * program[index+2]
-            }
-            if *opcode == 99 {
-                // let ret = format!("{:?}", program);
-                // println!("Program\n{}",ret);
+                println!("{}:Op p[{}]*p[{}] -> {}*{} replacing p[{}]-> {} with {}",
+                    index,
+                    op1,
+                    op2,
+                    program[op1],
+                    program[op2],
+                    result_pos,
+                    program[result_pos],
+                    program[op1]*program[op2]);
+                println!("OLD: {}", format!("{:?}", program));
+                program[result_pos] = program[op1] * program[op2];
+                println!("NEW: {}", format!("{:?}", program));
             }
         }
+            println!("Incrementing index: {}", index);
+            index += 4;
+            println!(" index: {}", index);
     }
 }
